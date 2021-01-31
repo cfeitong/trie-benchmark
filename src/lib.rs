1b190c9147556d3e8243cb3572ecd37947e45abe
Chen Feitong <cfeitong@gmail.com>
2021-01-31 20:51:56 +0800

    correct naive ac

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
-pub mod naive_trie; 
- pub mod naive_ac;
+pub mod naive_ac;
+pub mod naive_trie;
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
--- src/naive_ac.rs
+++ src/naive_ac.rs
@@ -58,43 +58,59 @@ impl<T> Node<T> {
 
 impl<T: AsRef<[u8]>> NaiveAcBuilder<T> {
     fn replace(&self, text: &[u8]) -> Vec<u8> {
-        let mut result = vec![];
-        let mut n = self.root;
-        let mut idx = 0;
-        let mut tar: Option<(usize, usize)> = None;
-        let mut last = 0;
-        while idx < text.len() {
-            if self.nodes[n].value.is_some() {
-                tar = Some((idx, n));
-            }
-            let edg = text[idx] as usize;
-            if let Some(nn) = self.nodes[n].next_node(edg) {
-                idx += 1;
-                n = nn;
-            } else if let Some((end, va)) = tar.take() {
-                let start = end - self.nodes[va].len;
-                result.extend_from_slice(&text[last..start]);
-                result.extend_from_slice(self.nodes[va].value.as_ref().expect("bug!").as_ref());
-                last = end;
-                idx = end;
-                n = self.root;
-            } else if n != self.root {
-                while n != self.root && !self.nodes[n].has_edg(edg) {
-                    n = self.nodes[n].fail;
-                }
+        let mut matches: Vec<(usize, usize, _)> = vec![];
+        let mut cur = self.root;
+        for idx in 0..=text.len() {
+            let edg = if idx == text.len() {
+                0
             } else {
-                idx += 1;
+                text[idx] as usize
+            };
+            loop {
+                if let Some(n) = self.nodes[cur].next_node(edg) {
+                    cur = n;
+                    break;
+                } else {
+                    if cur == self.root {
+                        break;
+                    }
+                    cur = self.nodes[cur].fail;
+                }
+            }
+            let mut p = cur;
+            while p != self.root {
+                if let Some(value) = &self.nodes[p].value {
+                    matches.push((idx + 1 - self.nodes[p].len, idx + 1, value));
+                }
+                p = self.nodes[p].fail;
             }
         }
-        if let Some((end, va)) = tar.take() {
-            let start = end - self.nodes[va].len;
-            result.extend_from_slice(&text[last..start]);
-            result.extend_from_slice(self.nodes[va].value.as_ref().expect("bug!").as_ref());
-            result.extend_from_slice(&text[end..]);
-        } else {
-            result.extend_from_slice(&text[last..]);
+        matches.sort_by(|a, b| {
+            if a.0 == b.0 {
+                a.1.cmp(&b.1)
+            } else {
+                a.0.cmp(&b.0)
+            }
+        });
+        let mut last = 0;
+        let mut ret = vec![];
+        let mut iter = matches.into_iter().peekable();
+        while let Some(v) = iter.next() {
+            let (start, end, value) = v;
+            if last > start
+                || iter
+                    .peek()
+                    .map(|&(s, e, _)| s == start && e > end)
+                    .unwrap_or(false)
+            {
+                continue;
+            }
+            ret.extend_from_slice(&text[last..start]);
+            ret.extend_from_slice(value.as_ref());
+            last = end;
         }
-        result
+        ret.extend_from_slice(&text[last..]);
+        ret
     }
 
     pub fn new() -> Self {
@@ -109,7 +125,7 @@ impl<T: AsRef<[u8]>> NaiveAcBuilder<T> {
         let mut data = key;
         while let Some((&edg, rest)) = data.split_first() {
             let edg = edg as usize;
-            cur = if self.nodes[cur].next[edg] == 0 {
+            cur = if !self.nodes[cur].has_edg(edg) {
                 let n = self.alloc_node(None);
                 self.nodes[cur].next[edg] = n;
                 self.nodes[n].len = self.nodes[cur].len + 1;
@@ -130,7 +146,7 @@ impl<T: AsRef<[u8]>> NaiveAcBuilder<T> {
     fn build_imp(&mut self) {
         let mut qu = VecDeque::new();
         qu.push_back(self.root);
-        while let Some(cur) = qu.pop_back() {
+        while let Some(cur) = qu.pop_front() {
             for (edg, n) in self.nodes[cur]
                 .next
                 .clone()
@@ -138,15 +154,19 @@ impl<T: AsRef<[u8]>> NaiveAcBuilder<T> {
                 .enumerate()
                 .filter(|(_, n)| *n != 0)
             {
+                if cur == self.root {
+                    self.nodes[n].fail = cur;
+                    qu.push_back(n);
+                    continue;
+                }
                 let mut fail = self.nodes[cur].fail;
                 while !self.nodes[fail].has_edg(edg) && fail != self.root {
                     fail = self.nodes[fail].fail;
                 }
-                self.nodes[n].fail = fail;
-                if cur != self.root {
-                    if let Some(nn) = self.nodes[fail].next_node(edg) {
-                        self.nodes[n].fail = nn;
-                    }
+                if let Some(nn) = self.nodes[fail].next_node(edg) {
+                    self.nodes[n].fail = nn;
+                } else {
+                    self.nodes[n].fail = self.root;
                 }
                 qu.push_back(n);
             }
@@ -161,6 +181,9 @@ impl<T: AsRef<[u8]>> NaiveAcBuilder<T> {
 
 #[cfg(test)]
 mod tests {
+    use std::io::BufRead;
+    use std::io::BufReader;
+
     use super::*;
 
     #[test]
@@ -203,5 +226,86 @@ mod tests {
         let result = ac.replace(text.as_bytes());
         let result_str = String::from_utf8_lossy(&result);
         assert_eq!(result_str, "123123445");
+    }
+
+    #[test]
+    fn test_naive_ac4() {
+        let text = "新变形金刚8";
+        let ac = build_dict_ac().expect("fail to build ac");
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "新*8");
+    }
+
+    fn build_dict_ac() -> anyhow::Result<NaiveAc<&'static str>> {
+        let dict = std::fs::File::open("ac/data/dict.txt")?;
+        let reader = BufReader::new(dict);
+        let mut ac = NaiveAcBuilder::new();
+        for line in reader.lines() {
+            let line = line?;
+            let key = line
+                .split_whitespace()
+                .next()
+                .expect("fail to parse dict line");
+            ac.insert(key.as_bytes(), "*");
+        }
+        Ok(ac.build())
+    }
+
+    #[test]
+    fn test_naive_ac5() {
+        let text = "1111111";
+        let mut ac = NaiveAcBuilder::new();
+        ac.insert("1111".as_bytes(), "*");
+        ac.insert("111".as_bytes(), "#");
+        let ac = ac.build();
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "*#");
+    }
+
+    #[test]
+    fn test_naive_ac6() {
+        let text = "111111121112";
+        let mut ac = NaiveAcBuilder::new();
+        ac.insert("1111".as_bytes(), "*");
+        ac.insert("1112".as_bytes(), "#");
+        let ac = ac.build();
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "*##");
+    }
+
+    #[test]
+    fn test_naive_ac7() {
+        let text = "爱一个人其实不容易";
+        let ac = build_dict_ac().expect("fail to build ac");
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "爱*其实不容易");
+    }
+
+    #[test]
+    fn test_naive_ac8() {
+        let text = "1234";
+        let mut ac = NaiveAcBuilder::new();
+        ac.insert("23".as_bytes(), "*");
+        ac.insert("12345".as_bytes(), "*");
+        let ac = ac.build();
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "1*4");
+    }
+
+    #[test]
+    fn test_naive_ac9() {
+        let text = "12345";
+        let mut ac = NaiveAcBuilder::new();
+        ac.insert("23".as_bytes(), "*");
+        ac.insert("1234".as_bytes(), "*");
+        let ac = ac.build();
+        let result = ac.replace(text.as_bytes());
+        let result_str = String::from_utf8_lossy(&result);
+        assert_eq!(result_str, "*5");
     }
 }
